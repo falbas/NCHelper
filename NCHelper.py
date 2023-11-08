e844c6b16a7d04484afb7c106e3e382eb9e92d6f
falbas <farhanalbas30@gmail.com>
2023-11-08 20:03:47 +0700

    commit

--- NCHelper.py
+++ NCHelper.py
@@ -9,23 +9,19 @@ from matplotlib.colors import ListedColormap
 
 
 class NCHelper:
-    def __init__(self, dataset, dir_path):
-        # read the dataset
+    def __init__(self, dataset=""):
+        if dataset == "":
+            return None
+
         print("reading dataset...")
         try:
             self.ds = xr.open_dataset(dataset)
+            print("reading dataset done")
         except:
             print("reading dataset failed")
             raise Exception("reading dataset failed")
-        print("reading dataset done")
-        
-        # create new directory
-        self.dir_path = dir_path
-        if os.path.exists(dir_path):
-            shutil.rmtree(dir_path)
-        os.mkdir(dir_path)
 
-    def nc2asc(self, var, lat, lon, asc_name):
+    def nc2asc(self, var, lat, lon, output_name):
         # prepare the variables
         ncols = len(lon.values)
         nrows = len(lat.values)
@@ -36,8 +32,9 @@ class NCHelper:
         grid = var.values
 
         # generate ascii grid file
-        print(f"generating {asc_name} ascii grid...")
-        with open(f"{self.dir_path}/{asc_name}.asc", "w") as file:
+        print(f"generating {output_name} ascii grid...")
+        output_file = f"{output_name}.asc"
+        with open(f"{output_file}", "w") as file:
             file.write(f"ncols {ncols}\n")
             file.write(f"nrows {nrows}\n")
             file.write(f"xllcorner {xllcorner}\n")
@@ -48,12 +45,12 @@ class NCHelper:
                 for j in i:
                     file.write(f"{j} ")
                 file.write("\n")
-        print(f"file: {os.getcwd()}/{self.dir_path}/{asc_name}.asc".replace("\\", "/"))
+        print(f"output: {os.getcwd()}/{output_file}".replace("\\", "/"))
 
-    def nc2image(self, var, img_name, x_dim, y_dim, cmap="turbo", flip=None):
+    def nc2image(self, var, output_name, x_dim, y_dim, cmap="turbo", flip=None):
         # generate gray geotiff image
-        print(f"generating {img_name} the images...")
-        img_dir = f"{self.dir_path}/{img_name}"
+        print(f"generating {output_name} the images...")
+        img_dir = f"{output_name}"
         _var = var.rio.set_spatial_dims(x_dim=x_dim, y_dim=y_dim)
         _var.rio.crs
         _var.rio.write_crs("epsg:4326", inplace=True)
@@ -73,7 +70,7 @@ class NCHelper:
                 lines = file.readlines()
                 for line in lines:
                     values = line.split()
-                    if (len(values) == 4):
+                    if len(values) == 4:
                         r, g, b = int(values[1]), int(values[2]), int(values[3])
                         custom_colors.append((r, g, b))
                     else:
@@ -149,18 +146,17 @@ class NCHelper:
         var_tiff_raster.close()
         dataset.close()
 
-    def geotiff2tiles(self, gdal2tiles, img_name, zoom="5-7", processes=1):
-        if not os.path.exists(f"{self.dir_path}/{img_name}_rgb.tiff"):
-            raise Exception("run nc2asc() first to generate the geotiff image")
-
+    def geotiff2tiles(
+        self, gdal2tiles_path, input_tiff, output_dir, zoom="5-7", processes=1
+    ):
         # generate the tiles with gdal2tiles.py
         # https://gdal.org/programs/gdal2tiles.html
         subprocess.run(
             [
                 "python",
-                gdal2tiles,
-                f"{self.dir_path}/{img_name}_rgb.tiff",
-                f"{self.dir_path}/tiles/{img_name}",
+                gdal2tiles_path,
+                f"{input_tiff}",
+                f"{output_dir}",
                 f"--zoom={zoom}",
                 f"--processes={processes}",
             ]
--- main.py
+++ main.py
@@ -3,22 +3,42 @@ from datetime import datetime, timedelta
 import time
 import os
 
-date = datetime.now().replace(hour=00)
-ds = None
+date = datetime.now().replace(hour=12)
+nch = None
+dir_path = ""
 while True:
     date_str = date.strftime("%Y%m%d%H")
     dir_path = f"storage/{date_str}"
     if os.path.exists(dir_path):
         print("already exists")
         break
-
+    
     try:
         nch = NCHelper(
-            f"http://182.16.248.173:8080/dods/INA-NWP/{date_str}/{date_str}-d01-asim",
-            dir_path=dir_path,
+            f"http://182.16.248.173:8080/dods/INA-NWP/{date_str}/{date_str}-d01-asim"
         )
+        os.mkdir(dir_path)
         break
     except:
+        print(date_str)
         date = date - timedelta(hours=12)
-        
     time.sleep(1)
+
+u = nch.ds["u"]
+v = nch.ds["v"]
+lat = nch.ds["lat"]
+lon = nch.ds["lon"]
+nch.nc2asc(u[0][0], lat, lon, f"{dir_path}/U")
+nch.nc2asc(v[0][0], lat, lon, f"{dir_path}/V")
+
+tc = nch.ds["tc"]
+tc_dir = f"{dir_path}/tc"
+os.mkdir(tc_dir)
+nch.nc2image(tc[0][0], f"{tc_dir}/tc", x_dim="lon", y_dim="lat", flip=0, cmap="turbo")
+nch.geotiff2tiles("C:/Users/falbas/miniconda3/Scripts/gdal2tiles.py", f"{tc_dir}/tc_rgb.tiff", f"{tc_dir}/tiles", "5-7", 16)
+
+psfc = nch.ds["psfc"]
+psfc_dir = f"{dir_path}/psfc"
+os.mkdir(psfc_dir)
+nch.nc2image(psfc[0][0], f"{psfc_dir}/psfc", x_dim="lon", y_dim="lat", flip=0, cmap="turbo")
+nch.geotiff2tiles("C:/Users/falbas/miniconda3/Scripts/gdal2tiles.py", f"{psfc_dir}/psfc_rgb.tiff", f"{psfc_dir}/tiles", "5-7", 16)
