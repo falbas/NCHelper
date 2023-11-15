0b84dc81b637d02731bbfdc4f89be9ccfec0f81a
falbas <farhanalbas30@gmail.com>
2023-11-16 00:24:31 +0700

    NCHelper v2

--- NCHelper.py
+++ NCHelper.py
@@ -1,12 +1,8 @@
 import os
-import shutil
 import subprocess
 import xarray as xr
-import rasterio
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-
+import rasterio
 
 class NCHelper:
     def __init__(self, dataset=""):
@@ -26,17 +22,17 @@ class NCHelper:
         var = var.values
         lat = lat.values
         lon = lon.values
-        if lat[0] < lat[-1]: # vertical
+        if lat[0] < lat[-1]:  # vertical
             lat = np.flip(lat, axis=0)
             var = np.flip(var, axis=0)
-        avlat = abs(lat[0]-lat[-1])/len(lat)
-        avlon = abs(lon[0]-lon[-1])/len(lon)
+        avlat = abs(lat[0] - lat[-1]) / len(lat)
+        avlon = abs(lon[0] - lon[-1]) / len(lon)
 
         ncols = len(lon)
         nrows = len(lat)
         xllcorner = lon[0]
         yllcorner = lat[-1]
-        cellsize = (avlat+avlon)/2
+        cellsize = (avlat + avlon) / 2
         NODATA_value = -9999
         grid = var
 
@@ -56,31 +52,51 @@ class NCHelper:
                 file.write("\n")
         print(f"output: {os.getcwd()}/{output_file}".replace("\\", "/"))
 
-    def nc2tiff(self, var, output_name, x_dim, y_dim, color_table):
+    def nc2tif(self, var, output_name, x_dim, y_dim):
         # generate gray geotiff image
         print(f"generating {output_name} geotiff images...")
         img_dir = f"{output_name}"
         _var = var.rio.set_spatial_dims(x_dim=x_dim, y_dim=y_dim)
         _var.rio.crs
         _var.rio.write_crs("epsg:4326", inplace=True)
-        _var.rio.to_raster(f"{img_dir}.tiff")
+        _var.rio.to_raster(f"{img_dir}.tif")
 
-        # generate rgb geotiff image
+    def nc2tif2(self, var, output_name, lat, lon, nodata=9999):
+        # generate gray geotif image
+        var = np.flip(var, axis=0)
+        transform = rasterio.transform.from_bounds(west=lon[0],south=lat[0],east=lon[-1],north=lat[-1],width=lon.shape[0],height=lat.shape[0])
+        with rasterio.open(
+            f"{output_name}.tif",
+            "w",
+            driver="GTiff",
+            height=lat.shape[0],
+            width=lon.shape[0],
+            count=1,
+            dtype=var.dtype,
+            crs="+proj=latlong",
+            transform=transform,
+            nodata=nodata
+        ) as dst:
+            dst.write(var, 1)
+
+    def nc2rgbtif(self, output_name, color_table):
+        img_dir = f"{output_name}"
+        # generate rgb geotif image
+        # https://gdal.org/programs/gdaldem.html
+        print(f"generating {output_name} rgb geotif images...")
         subprocess.run(
             [
                 "gdaldem.exe",
                 "color-relief",
-                f"{img_dir}.tiff",
+                f"{img_dir}.tif",
                 color_table,
-                f"{img_dir}_rgb.tiff",
+                f"{img_dir}_rgb.tif",
             ]
         )
+        print(f"output: {os.getcwd()}/{img_dir}_rgb.tif".replace("\\", "/"))
 
-        print(f"output: {os.getcwd()}/{img_dir}.tiff".replace("\\", "/"))
-        print(f"output: {os.getcwd()}/{img_dir}_rgb.tiff".replace("\\", "/"))
-
-    def geotiff2tiles(
-        self, gdal2tiles_path, input_tiff, output_dir, zoom="5-7", processes=1
+    def geotif2tiles(
+        self, gdal2tiles_path, input_tif, output_dir, zoom="5-7", processes=1
     ):
         # generate the tiles with gdal2tiles.py
         # https://gdal.org/programs/gdal2tiles.html
@@ -88,7 +104,7 @@ class NCHelper:
             [
                 "python",
                 gdal2tiles_path,
-                f"{input_tiff}",
+                f"{input_tif}",
                 f"{output_dir}",
                 f"--zoom={zoom}",
                 f"--processes={processes}",
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from NCHelper import NCHelper
 import os
 import shutil
 
-initial_times = ["2023110612", "2023110700", "2023110800"]
+initial_times = ["2023110800"]
 
 for initial_time in initial_times:
     nch = NCHelper(
@@ -17,19 +17,19 @@ for initial_time in initial_times:
 
     level = nch.ds["lev"]
     # levels = [0, 3, 6, 10, 16]
-    levels = [0, 3, 6]
+    levels = [0]
 
     lat = nch.ds["lat"]
     lon = nch.ds["lon"]
     u = nch.ds["u"]
     v = nch.ds["v"]
 
-    vars = ["wspd", "tc", "rh"]
+    vars = ["u", "v"]
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
     output_dir = f"storage/{initial_time}"
     os.mkdir(output_dir)
-    for i in range(0, len(times)):
+    for i in range(0, len(times)-24):
         dir_path = f"{output_dir}/{times[i]}"
         if os.path.exists(dir_path):
             shutil.rmtree(dir_path)
@@ -41,23 +41,23 @@ for initial_time in initial_times:
 
             wind_dir = f"{dir_path}/wind"
             os.mkdir(wind_dir)
-            nch.nc2asc(u[i][j], lat, lon, f"{wind_dir}/U")
-            nch.nc2asc(v[i][j], lat, lon, f"{wind_dir}/V")
+            # nch.nc2asc(u[i][j], lat, lon, f"{wind_dir}/U")
+            # nch.nc2asc(v[i][j], lat, lon, f"{wind_dir}/V")
 
-            # for var in vars:
-            #     var_dir = f"{dir_path}/{var}"
-            #     os.mkdir(var_dir)
-            #     nch.nc2tiff(
-            #         nch.ds[var][i][j],
-            #         f"{var_dir}/{var}",
-            #         x_dim="lon",
-            #         y_dim="lat",
-            #         color_table=f"{current_dir}/{var}_color.txt",
-            #     )
-            #     nch.geotiff2tiles(
-            #         "C:/Users/falbas/miniconda3/Scripts/gdal2tiles.py",
-            #         f"{var_dir}/{var}_rgb.tiff",
-            #         f"{var_dir}/tiles",
-            #         "5-8",
-            #         16,
-            #     )
+            for var in vars:
+                var_dir = f"{dir_path}/{var}"
+                os.mkdir(var_dir)
+                nch.nc2tiff(
+                    nch.ds[var][i][j],
+                    f"{var_dir}/{var}",
+                    x_dim="lon",
+                    y_dim="lat",
+                    color_table=f"{current_dir}/{var}_color.txt",
+                )
+                nch.geotiff2tiles(
+                    "C:/Users/falbas/miniconda3/Scripts/gdal2tiles.py",
+                    f"{var_dir}/{var}_rgb.tiff",
+                    f"{var_dir}/tiles",
+                    "5-8",
+                    16,
+                )
